825248aa63a9010ff945665443d5c5a393875ae2
david11yf29 <david11yf29@gmail.com>
2020-09-08 19:35:13 +0800

    updated

--- dist/controllers/todos.js
+++ dist/controllers/todos.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTodo = void 0;
+exports.updateTodo = exports.getTodos = exports.createTodo = void 0;
 const todo_1 = require("../models/todo");
 const TODOS = [];
 exports.createTodo = (req, res, next) => {
@@ -8,4 +8,17 @@ exports.createTodo = (req, res, next) => {
     const newTodo = new todo_1.Todo(Math.random().toString(), text);
     TODOS.push(newTodo);
     res.status(201).json({ message: 'Create the todo.', createdTodo: newTodo });
+};
+exports.getTodos = (req, res, next) => {
+    res.json({ todos: TODOS });
+};
+exports.updateTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const updatedText = req.body.text;
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error('Cound not find todo!');
+    }
+    TODOS[todoIndex] = new todo_1.Todo(TODOS[todoIndex].id, updatedText);
+    res.json({ message: 'Updated!', updatedTodo: TODOS[todoIndex] });
 };
